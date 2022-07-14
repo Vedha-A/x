54039805d8380dc480dd7f0fe55ea2c2471c8b20
Vedha-A <85233825+Vedha-A@users.noreply.github.com>
2022-07-14 18:44:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -28,8 +28,8 @@ function gotResults(error, results) {
       var utterThis = new SpeechSynthesisUtterance(speak_data);
       synth.speak(utterThis);
 
-      document.getElementById("result_object_name"),innerHTML = results[0].label;
-      document.getElementById("result_object_accuracy"),innerHTML = results[0].confidence.toFixed(3);
+      document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
 
     
